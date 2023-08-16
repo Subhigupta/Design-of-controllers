6a016b209f638ea234d1ebae7b6ac0bbd19ce3e9
Subhigupta <subhiguptasfc@gmail.com>
2023-08-16 19:12:29 +0530

    Added comments to the controllability codes and deleted unnecessary files.

--- Controllability_of_a_Linear_System.m
+++ Controllability_of_a_Linear_System.m
@@ -1,9 +1,12 @@
 %Demostrate controllability
 clear all
 clc
-%% %% Example 4: System is controllable with a single input
+
+%In both the cases cntrol input has been applied at 45 degree to the block.
+%% Example 4: System is controllable with a single input
 
 m=1;
+%the horizontal and vertical spring coefficients are different.
 k1=1;
 k2=4;
 
@@ -18,13 +21,16 @@ B=[0;
     1/m];
 
 Pc=ctrb(A,B);
-rank(Pc)
+rank(Pc) % the rank came out be 4
+%All the states are controllable and hence on ruuning the simulation of the
+%system the states should reach the arbitrary state independently
 
 [V,D]=eig(A)
 
 %% Example 5: System is uncontrollable with a single input
 
 m=1;
+%the horizontal and vertical spring coefficients are same.
 k1=1/3;
 k2=1/3;
 
