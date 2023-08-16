47939e549ca2683e50acd1a522dff4af3db672bc
Subhigupta <subhiguptasfc@gmail.com>
2023-08-16 19:22:28 +0530

    Added the comments to FSFB and PBH codes.

--- Introduction_to_Full_State_Feedback_COntrol.m
+++ Introduction_to_Full_State_Feedback_COntrol.m
@@ -1,6 +1,9 @@
-%% Example 1: System is controllable
 clear all
 clc
+%Both examples demonstrate that with place command it is easy to design
+%full state feedback control which will place the open loop poles to
+%desired poles.
+%% Example 1: System is controllable
 
 A=[0 3;2 4];
 B=[-2;1];
